a60014a0058f19bfb44512226f7ec69e093425c5
Katarn34 <134236396+Katarn34@users.noreply.github.com>
2023-12-19 16:09:06 +0100

    :)

--- Assets/Script/MainBrick.cs
+++ Assets/Script/MainBrick.cs
@@ -6,23 +6,21 @@ public class MainBrick : MonoBehaviour
 {
     public int points;
     public GameManager Gm;
-    public Transform powerupLife;
-    public Transform powerupSize;
+    public Transform powerup;
     public Transform Explosion;
+    public int Chance;
    private void OnCollisionEnter2D(Collision2D _collision)
     {
         //If the ball touch the bricks 50% chance of getting the extra life power up and 25% for the Extra size
         //Updating the score and setting it for each bricks and destroying the brick
         if (_collision.gameObject.tag == "Ball")
         {
-            int randChance = Random.Range(1, 101);
-            if(randChance < 50)
-                Instantiate(powerupLife,transform.position,transform.rotation);
+            int randChance = Random.Range(1, 100);
+            if(randChance < Chance)
+                Instantiate(powerup,transform.position,transform.rotation);
             {
 
             }
-            if (randChance < 25)
-                Instantiate(powerupSize, transform.position, transform.rotation);
 
             Gm.UpdateScore(points);
             Gm.UpdateNumberOfBricks();
